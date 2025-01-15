af0e52c30258250db9f739d23cbe877b259c8b3c
RLH-2110 <roland.hartung@gmx.net>
2025-01-15 15:12:58 +0100

    fixed string comparrision
    fixed it on windows and added the new check to linux

--- main_win.c
+++ main_win.c
@@ -44,16 +44,16 @@
 			return 0;
 		}
 
+		if (url[0] == '\0') {
+			putslog("gave an empty url!");
+			return 0;
+		}
 
 		if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
 		    perrorlog("WSAStartup failed");
 		    return 0;
 		}
 
-		if (url == "") {
-			putslog("gave an empty url!");
-			return 0;
-		}
 
 		for(DNSindex = 0;;DNSindex++){
 
